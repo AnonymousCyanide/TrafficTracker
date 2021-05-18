f3072617a8e4da5354e5e59c413bb9f8f34e7760
Avi <avidubey2000@gmail.com>
2021-05-18 07:03:20 +0530

    shows moving object greater than 100px

--- main.py
+++ main.py
@@ -12,11 +12,20 @@ while True:
     ret , frame  = cap.read()
     # Marks moving vehicals as white
     mask = vehical_detector.apply(frame)
+    # vehical detection
+    contours , _ = cv2.findContours(mask , cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        # Calculate are
+        area = cv2.contourArea(cnt)
+        if area>100:
+         cv2.drawContours(frame , [cnt] , -1,(0 ,0 ,255), 1)
+        
+
     # projects current image in media window named Frame
     cv2.imshow('Frame',frame)
-    cv2.imshow('Mask',mask)
+    #cv2.imshow('Mask',mask)
 
-    key = cv2.waitKey(0)
+    key = cv2.waitKey(60)
     # Progamr stops on esc
     if(key == 27):
         break
