26b783ef9ae3e3dbaad193a9ac9652c414b3a64d
Avi <avidubey2000@gmail.com>
2021-05-18 11:10:20 +0530

    Improved detection using history and threshold in bkg subtractor

--- main.py
+++ main.py
@@ -4,25 +4,29 @@ cap = cv2.VideoCapture('highway.mp4')
 
 # Detect vehicals 
 # checks for moving objects in this context vehicals
-vehical_detector = cv2.createBackgroundSubtractorMOG2()
+vehical_detector = cv2.createBackgroundSubtractorMOG2(history =100 , varThreshold =40)
 
 
 while True:
     # stores current frame in frame
     ret , frame  = cap.read()
+    # get road
+    road = frame[340:720 ,500:800]
     # Marks moving vehicals as white
-    mask = vehical_detector.apply(frame)
+    mask = vehical_detector.apply(road)
     # vehical detection
     contours , _ = cv2.findContours(mask , cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
     for cnt in contours:
         # Calculate are
         area = cv2.contourArea(cnt)
-        if area>100:
-         cv2.drawContours(frame , [cnt] , -1,(0 ,0 ,255), 1)
+        if area>200:
+         cv2.drawContours(road , [cnt] , -1,(0 ,0 ,255), 1)
         
 
     # projects current image in media window named Frame
     cv2.imshow('Frame',frame)
+    cv2.imshow('Road',road)
     #cv2.imshow('Mask',mask)
 
     key = cv2.waitKey(60)
